2fedfd5c4d51bcaf6e3fc6d796883a1e4718bd86
m2enu <m2enu915vogel@gmail.com>
2021-05-05 03:08:38 +0900

    add Doxygen comment, fix include for `size_t`.

--- src/circular_buffer.c
+++ src/circular_buffer.c
@@ -1,19 +1,46 @@
+/**
+ * @file        circular_buffer.c
+ * @brief       Circular buffer for embedded systems.
+ * @author      m2enu
+ * @date        2021/05/05
+ * @copyright   Copyright (c) 2021 m2enu
+ * @par         License
+ *              https://github.com/m2enu/circular_buffer/blob/main/LICENSE
+ */
 #include <stdlib.h>
 #include "circular_buffer.h"
 
 static void circular_buffer_memset(uint8_t * dst, uint8_t src, size_t length);
 static void circular_buffer_memcpy(uint8_t * dst, const uint8_t * src, size_t length);
 
+/**
+ * @brief       Get smaller value between specified values
+ * @param[in]   a       Value 1
+ * @param[in]   b       Value 2
+ * @return      Smaller value
+ */
 static inline size_t min(size_t a, size_t b)
 {
     return (a < b) ? a: b;
 }
 
+/**
+ * @brief       Convert specified length to circular buffer index.
+ * @param[in]   length  Circular buffer index that might be exceeds its size
+ * @return      Circular buffer index
+ */
 static inline size_t buffer_size(size_t length)
 {
     return length & CIRCULAR_BUFFER_SIZE_MASK;
 }
 
+/**
+ * @brief       Set specified value to destination buffer.
+ * @param[out]  dst     Pointer to destination buffer
+ * @param[in]   src     Value to be set
+ * @param[in]   length  Length of setting
+ * @return      nothing
+ */
 static void circular_buffer_memset(uint8_t * dst, uint8_t src, size_t length)
 {
     size_t i;
@@ -23,6 +50,13 @@ static void circular_buffer_memset(uint8_t * dst, uint8_t src, size_t length)
     }
 }
 
+/**
+ * @brief       Copy buffer value from source to destination.
+ * @param[out]  dst     Pointer to destination buffer
+ * @param[in]   src     Pointer to source buffer
+ * @param[in]   length  Length of copying
+ * @return      nothing
+ */
 static void circular_buffer_memcpy(uint8_t * dst, const uint8_t * src, size_t length)
 {
     size_t i;
@@ -32,6 +66,11 @@ static void circular_buffer_memcpy(uint8_t * dst, const uint8_t * src, size_t le
     }
 }
 
+/**
+ * @brief       Initialize circular buffer
+ * @param[out]  p_buf   Pointer to circular buffer
+ * @return      nothing
+ */
 void circular_buffer_init(circular_buffer_t * p_buf)
 {
     if (p_buf == NULL)
@@ -44,6 +83,15 @@ void circular_buffer_init(circular_buffer_t * p_buf)
     p_buf->dequeue = 0;
 }
 
+/**
+ * @brief       Push data array to circular buffer
+ * @param[in]   p_buf   Pointer to circular buffer
+ * @param[in]   p_data  Pointer data to source data array
+ * @param[in]   length  Length of `p_data`
+ * @return      Success or failed as boolean
+ * @retval      (true)  Failed
+ * @retval      (false) Succeeded
+ */
 bool circular_buffer_enqueue(
     circular_buffer_t * p_buf,
     const uint8_t * p_data,
@@ -69,6 +117,15 @@ bool circular_buffer_enqueue(
     return false;
 }
 
+/**
+ * @brief       Pop data array from circular buffer
+ * @param[in]   p_buf   Pointer to circular buffer
+ * @param[out]  p_data  Pointer data to destination data array
+ * @param[in]   length  Length of `p_data`
+ * @return      Success or failed as boolean
+ * @retval      (true)  Failed
+ * @retval      (false) Succeeded
+ */
 bool circular_buffer_dequeue(
     circular_buffer_t * p_buf,
     uint8_t * p_data,
--- src/circular_buffer.h
+++ src/circular_buffer.h
@@ -1,24 +1,51 @@
+/**
+ * @file        circular_buffer.h
+ * @brief       Circular buffer for embedded systems.
+ * @author      m2enu
+ * @date        2021/05/05
+ * @copyright   Copyright (c) 2021 m2enu
+ * @par         License
+ *              https://github.com/m2enu/circular_buffer/blob/main/LICENSE
+ */
 #ifndef CIRCULAR_BUFFER_H_INCLUDED
 #define CIRCULAR_BUFFER_H_INCLUDED
 
 #include <stdint.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <sys/types.h>
 
+/**
+ * @brief       Circular buffer size definition
+ */
 #define CIRCULAR_BUFFER_SIZE        (128)
+
+/**
+ * @brief       Mask pattern for circular buffer index
+ */
 #define CIRCULAR_BUFFER_SIZE_MASK   (CIRCULAR_BUFFER_SIZE - 1)
 
 #if (CIRCULAR_BUFFER_SIZE & CIRCULAR_BUFFER_SIZE_MASK) != 0
 #error "CIRCULAR_BUFFER_SIZE must be a power of 2."
 #endif
 
+/**
+ * @brief       Circular buffer structure
+ */
 typedef struct circular_buffer_t_tag
 {
+    /** Entity of buffer */
     uint8_t     buffer[CIRCULAR_BUFFER_SIZE];
+    /** Pushed size */
     size_t      enqueue;
+    /** Size that will be poped */
     size_t      dequeue;
 } circular_buffer_t;
 
+/**
+ * @brief       Get pushed data count of specified buffer
+ * @param[in]   p_buf   Pointer to circular buffer
+ * @return      Pushed data count
+ */
 static inline size_t circular_buffer_count(
     const circular_buffer_t * p_buf)
 {
@@ -33,31 +60,75 @@ static inline size_t circular_buffer_count(
     return p_buf->enqueue - p_buf->dequeue;
 }
 
+/**
+ * @brief       Return true if specified buffer is available (able to push)
+ * @param[in]   p_buf   Pointer to circular buffer
+ * @return      The buffer is available or not
+ * @retval      (true)  The buffer is available
+ * @retval      (false) The buffer is not available
+ */
 static inline size_t circular_buffer_available(
     const circular_buffer_t * p_buf)
 {
     return (CIRCULAR_BUFFER_SIZE - circular_buffer_count(p_buf));
 }
 
+/**
+ * @brief       Return true if specified buffer is empty
+ * @param[in]   p_buf   Pointer to circular buffer
+ * @return      The buffer is empty or not
+ * @retval      (true)  The buffer is empty
+ * @retval      (false) The buffer is not empty
+ */
 static inline bool circular_buffer_is_empty(
     const circular_buffer_t * p_buf)
 {
     return (circular_buffer_count(p_buf) == 0);
 }
 
+/**
+ * @brief       Return true if specified buffer is full
+ * @param[in]   p_buf   Pointer to circular buffer
+ * @return      The buffer is full or not
+ * @retval      (true)  The buffer is full
+ * @retval      (false) The buffer is not full
+ */
 static inline bool circular_buffer_is_full(
     const circular_buffer_t * p_buf)
 {
     return (circular_buffer_available(p_buf) == 0);
 }
 
+/**
+ * @brief       Initialize circular buffer
+ * @param[out]  p_buf   Pointer to circular buffer
+ * @return      nothing
+ */
 void circular_buffer_init(circular_buffer_t * p_buf);
 
+/**
+ * @brief       Push data array to circular buffer
+ * @param[in]   p_buf   Pointer to circular buffer
+ * @param[in]   p_data  Pointer data to source data array
+ * @param[in]   length  Length of `p_data`
+ * @return      Success or failed as boolean
+ * @retval      (true)  Failed
+ * @retval      (false) Succeeded
+ */
 bool circular_buffer_enqueue(
     circular_buffer_t * p_buf,
     const uint8_t * data,
     size_t length);
 
+/**
+ * @brief       Pop data array from circular buffer
+ * @param[in]   p_buf   Pointer to circular buffer
+ * @param[out]  p_data  Pointer data to destination data array
+ * @param[in]   length  Length of `p_data`
+ * @return      Success or failed as boolean
+ * @retval      (true)  Failed
+ * @retval      (false) Succeeded
+ */
 bool circular_buffer_dequeue(
     circular_buffer_t * p_buf,
     uint8_t * p_data,
